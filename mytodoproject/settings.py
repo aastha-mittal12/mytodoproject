552b542cefc0076f1f378753ec0ab167b4776156
Aastha Mittal <aastha.mittal@compup.io>
2025-08-31 19:00:36 +0530

    add static media files

--- mytodoproject/settings.py
+++ mytodoproject/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'  # This is where collectstatic will gather all files
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'  # This is where uploaded media files will be stored
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
